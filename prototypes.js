acb9507a78dcf02f505ad87aa8a45d3a5576c9fd
Luka Goggins <lukarowangoggins@computingforall.org>
2021-10-29 16:06:18 -0700

    done

--- prototypes.js
+++ prototypes.js
@@ -10,9 +10,23 @@ function Animal(numberOfLegs, numberOfEyes)
 //     - Breed can be set upon creation
 //     - Inherits from the Animal object
 
+function dog(breed){
+	function bark(){
+		console.log("i am barking");	
+	}
+	this.breed = breed;
+	this.bark = bark;
+}
+
+dog.prototype = new Animal(4,2);
+
 // (2) Create a new dog from that object constructor
+
+let realDog = new dog("realDogISwear");
 
 // (3) Console log the dog's number of legs, number of
 //     eyes, and breed
+console.log(realDog.legs, realDog.eyes, realDog.breed);
 
 // (4) Make the dog bark
+realDog.bark();
