5b2f4afbbb186e6ea148d3106d7595d0b148af3f
Jonathan C <jsea@uw.edu>
2021-10-29 15:07:55 -0700

    Added assignment files

---
+++ prototypes.js
@@ -0,0 +1,18 @@
+function Animal(numberOfLegs, numberOfEyes)
+{
+	this.legs = numberOfLegs;
+	this.eyes = numberOfEyes;
+}
+
+// (1) Create a new object constructor for a Dog that
+//     meets the following criteria
+//     - Barks in the console
+//     - Breed can be set upon creation
+//     - Inherits from the Animal object
+
+// (2) Create a new dog from that object constructor
+
+// (3) Console log the dog's number of legs, number of
+//     eyes, and breed
+
+// (4) Make the dog bark
